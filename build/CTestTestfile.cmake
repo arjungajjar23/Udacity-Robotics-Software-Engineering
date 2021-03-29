b46099aa44fc99b27a63ab475d8665b4d408fe99
arjungajjar23 <arjun.gajjar23@gmail.com>
2021-03-29 02:10:19 +0000

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/workspace/catkin_ws/src
+# Build directory: /home/workspace/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(Project_2/my_robot)
+subdirs(Project_2/ball_chaser)
+subdirs(teleop_twist_keyboard)
+subdirs(where_am_i)
+subdirs(pgm_map_creator)
